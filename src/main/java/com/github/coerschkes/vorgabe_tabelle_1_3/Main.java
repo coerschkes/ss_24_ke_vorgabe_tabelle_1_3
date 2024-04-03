2e8fff1b59042c23526963b592f957516dd96bd9
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-04-03 15:30:15 +0200

    feat: finish the base layout.

--- src/main/java/com/github/coerschkes/vorgabe_tabelle_1_3/Main.java
+++ src/main/java/com/github/coerschkes/vorgabe_tabelle_1_3/Main.java
@@ -11,15 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Der Titel des ersten Fensters (primaryStage) wird gesetzt.
         primaryStage.setTitle("User system persons");
-        // Die fxml-Datei, welche die GUI-Daten
-        // zu dem Fenster enthaelt, wird geladen.
         final FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("userSystemView.fxml"));
-        // Eine grundlegende Scheibe (pane) fuer die GUI wird geladen.
         final BorderPane root = loader.load();
-        // Setzen des Scenegraphen
         Scene scene = new Scene(root, 480, 240);
         primaryStage.setScene(scene);
         primaryStage.show();
